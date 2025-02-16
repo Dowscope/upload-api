762873e6042e90923a034dcf02138bc15dae7bba
Timothy Dowling <timothy.dowling@me.com>
2025-02-16 13:22:05 -0500

    correct typo

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ app.get('/list_music', function(req, res) {
 });
 
 app.get('/checkUser', function(req, res) {
-  const (username, password) = req.body;
+  const { username, password } = req.body;
 
   if (!username || !password) {
     return res.status(400).json({ error: 'Username and password are required' });
