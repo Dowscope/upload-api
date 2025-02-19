f7bbbfced0b1a6fb43400500de0e9087b25ea372
Timothy Dowling <timothy.dowling@me.com>
2025-02-18 21:12:36 -0500

    Update

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ app.post('/rtsstatus', (req, res) => {
       try {
         const url = "http://192.168.0.113/status";
         console.log(url);
-        axios.get(url, (re, rs) => {
+        await axios.get(url, (re, rs) => {
           console.log(rs.body);
           res.json({status: rs.body});
         });
