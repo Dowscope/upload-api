ad48f155031b9c4eb0060696a623864d1a20ec33
Timothy Dowling <timothy.dowling@me.com>
2025-02-26 20:45:58 -0500

    Update

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function isValidEmail(email) {
 app.use(cors({
   origin: ['http://localhost:55000', 'http://dowscopemedia.ca', 'http://localhost:8080'],
   methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin']
 }));
 
 const fileFilter = function(req, file, cb){
